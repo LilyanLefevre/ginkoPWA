12bb171a05e00c3fb670578d659cef7ea7ac6544
lilyanlefevre <lilyandu70.7@gmail.com>
2021-11-22 10:38:09 +0100

    add service worker

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
  *                                                                      *
  ************************************************************************/
 document.addEventListener("DOMContentLoaded", function (_e) {
-
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js');
+    };
     
     /******************************************************************
             Fonctions à compléter dans la dernière partie du TP 
